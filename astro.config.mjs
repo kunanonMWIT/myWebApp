f9b8f38e921613bf6fcc874afa7126bb3df5ae88
KunanonMWIT <kunanon.kha_g34@mwit.ac.th>
2026-07-15 20:32:46 +0700

    Feat: Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kunanonMWIT.github.io',
+  base: '/myWebApp',
+
   integrations: [solidJs()],
 
   vite: {
